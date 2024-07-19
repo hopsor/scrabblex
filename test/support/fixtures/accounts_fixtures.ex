38bc500d1933faeb4d4263fc58cd405186cb1b1b
Víctor Viruete <hopsor@gmail.com>
2024-07-19 15:23:02 +0200

    add user name to accounts fixtures

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,13 @@ defmodule Scrabblex.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
+  def unique_user_name, do: "u#{System.unique_integer([:positive])}"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password()
+      password: valid_user_password(),
+      name: unique_user_name()
     })
   end
 
