d8296ea9c8907371f2c6f9432c69456b29c945fe
Víctor Viruete <hopsor@gmail.com>
2024-08-17 20:15:23 +0200

    mix format

--- lib/scrabblex_web/live/match_live/lobby_component.ex
+++ lib/scrabblex_web/live/match_live/lobby_component.ex
@@ -41,7 +41,7 @@ defmodule ScrabblexWeb.MatchLive.LobbyComponent do
                 :if={lobby_user.owner}
                 class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
               >
-                Owner 
+                Owner
               </span>
             </div>
 
