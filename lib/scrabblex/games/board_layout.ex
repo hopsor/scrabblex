5266a5035cbd6eef117f9f8767c9edbd23aadfb3
Víctor Viruete <hopsor@gmail.com>
2024-08-22 01:56:50 +0200

    adds BoardLayout module

---
+++ lib/scrabblex/games/board_layout.ex
@@ -0,0 +1,28 @@
+defmodule Scrabblex.Games.BoardLayout do
+  def get() do
+    [
+      ["3W", nil, nil, "2L", nil, nil, nil, "3W", nil, nil, nil, "2L", nil, nil, "3W"],
+      [nil, "2W", nil, nil, nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "2W", nil],
+      [nil, nil, "2W", nil, nil, nil, "2L", nil, "2L", nil, nil, nil, "2W", nil, nil],
+      ["2L", nil, nil, "2W", nil, nil, nil, "2L", nil, nil, nil, "2W", nil, nil, "2L"],
+      [nil, nil, nil, nil, "2W", nil, nil, nil, nil, nil, "2W", nil, nil, nil, nil],
+      [nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "3L", nil],
+      [nil, nil, "2L", nil, nil, nil, "2L", nil, "2L", nil, nil, nil, "2L", nil, nil],
+      ["3W", nil, nil, "2L", nil, nil, nil, "X", nil, nil, nil, "2L", nil, nil, "3W"],
+      [nil, nil, "2L", nil, nil, nil, "2L", nil, "2L", nil, nil, nil, "2L", nil, nil],
+      [nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "3L", nil],
+      [nil, nil, nil, nil, "2W", nil, nil, nil, nil, nil, "2W", nil, nil, nil, nil],
+      ["2L", nil, nil, "2W", nil, nil, nil, "2L", nil, nil, nil, "2W", nil, nil, "2L"],
+      [nil, nil, "2W", nil, nil, nil, "2L", nil, "2L", nil, nil, nil, "2W", nil, nil],
+      [nil, "2W", nil, nil, nil, "3L", nil, nil, nil, "3L", nil, nil, nil, "2W", nil],
+      ["3W", nil, nil, "2L", nil, nil, nil, "3W", nil, nil, nil, "2L", nil, nil, "3W"]
+    ]
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {row, index_row} ->
+      Enum.with_index(row)
+      |> Enum.map(fn {value, index_column} ->
+        {index_row, index_column, value}
+      end)
+    end)
+  end
+end
---
+++ test/scrabblex/games/board_layout_test.exs
@@ -0,0 +1,19 @@
+defmodule Scrabblex.Games.BoardLayoutTest do
+  use ExUnit.Case
+
+  alias Scrabblex.Games.BoardLayout
+
+  describe "get/0" do
+    test "returns a list of 15*15 tuples with {x, y, slot_type}" do
+      result = BoardLayout.get()
+
+      assert length(result) == 15*15
+
+      Enum.each(result, fn {x, y, value} ->
+        assert x >= 0 && x < 15
+        assert y >= 0 && y < 15
+        assert value in ["3W", "2W", "3L", "2L", "X", nil]
+      end)
+    end
+  end
+end
