19f34bc6778b1d21d3b69ae6a4168b648119dcaa
Víctor Viruete <hopsor@gmail.com>
2024-09-03 02:39:36 +0200

    PlayValidator refactor. Moved Position to its own module

--- lib/scrabblex/games/tile.ex
+++ lib/scrabblex/games/tile.ex
@@ -2,26 +2,20 @@ defmodule Scrabblex.Games.Tile do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Scrabblex.Games.Position
+
   embedded_schema do
     field :score, :integer
     field :value, :string
     field :wildcard, :boolean
 
-    embeds_one :position, Position, on_replace: :delete, primary_key: false do
-      field :row, :integer
-      field :column, :integer
-    end
+    embeds_one :position, Position, on_replace: :delete
   end
 
   def changeset(tile, attrs) do
     tile
     |> cast(attrs, [:score, :value, :wildcard])
-    |> cast_embed(:position, with: &position_changeset/2)
+    |> cast_embed(:position)
     |> validate_required([:score, :value, :wildcard])
-  end
-
-  defp position_changeset(schema, params) do
-    schema
-    |> cast(params, [:row, :column])
   end
 end
