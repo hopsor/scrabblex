5249006839c24143443ee22bb9351f22fc65b283
Víctor Viruete <hopsor@gmail.com>
2024-09-18 17:20:00 +0200

    added optional strict mode Bag.draw_tiles

--- test/scrabblex/games/bag_test.exs
+++ test/scrabblex/games/bag_test.exs
@@ -28,7 +28,7 @@ defmodule Scrabblex.Games.BagTest do
     end
   end
 
-  describe "draw_tiles/2" do
+  describe "draw_tiles/3 with strict: false" do
     test "when the bag has enough it returns a tuple with the demand and the remainder" do
       bag = [%Tile{id: "1"}, %Tile{id: "2"}, %Tile{id: "3"}, %Tile{id: "4"}, %Tile{id: "5"}]
 
@@ -45,6 +45,26 @@ defmodule Scrabblex.Games.BagTest do
       bag = []
 
       assert {:ok, [], []} = Bag.draw_tiles(bag, 2)
+    end
+  end
+
+  describe "draw_tiles/3 with strict: true" do
+    test "when the bag has enough it returns a tuple with the demand and the remainder" do
+      bag = [%Tile{id: "1"}, %Tile{id: "2"}, %Tile{id: "3"}, %Tile{id: "4"}, %Tile{id: "5"}]
+
+      assert {:ok, [_, _], [_, _, _]} = Bag.draw_tiles(bag, 2, strict: true)
+    end
+
+    test "when the bag hasn't enough tiles it returns the remainder " do
+      bag = [%Tile{id: "1"}, %Tile{id: "2"}]
+
+      assert {:error, :demand_exceeded} = Bag.draw_tiles(bag, 3, strict: true)
+    end
+
+    test "when the bag is empty it returns two empty lists" do
+      bag = []
+
+      assert {:error, :demand_exceeded} = Bag.draw_tiles(bag, 2, strict: true)
     end
   end
 end
