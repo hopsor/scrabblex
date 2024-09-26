f51a864236d67a44e709e4ee9a4af8e1d6299686
Víctor Viruete <hopsor@gmail.com>
2024-09-27 00:53:44 +0200

    added :friendly_id column to existing match migration

--- priv/repo/migrations/20240712162817_create_matches.exs
+++ priv/repo/migrations/20240712162817_create_matches.exs
@@ -7,8 +7,11 @@ defmodule Scrabblex.Repo.Migrations.CreateMatches do
       add :bag, :map
       add :lexicon_id, references(:lexicons, on_delete: :nothing), null: false
       add :turn, :integer, default: 0
+      add :friendly_id, :string, null: false
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:matches, :friendly_id)
   end
 end
