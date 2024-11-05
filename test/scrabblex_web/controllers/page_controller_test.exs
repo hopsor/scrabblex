f0492b707700e04d42bac4cc763aff06f2928720
Víctor Viruete <hopsor@gmail.com>
2024-11-05 18:24:28 +0100

    fix test

--- test/scrabblex_web/controllers/page_controller_test.exs
+++ test/scrabblex_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule ScrabblexWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+
+    assert html_response(conn, 200) =~
+             "A web based Scrabble-like game built with Elixir and Phoenix LiveView"
   end
 end
