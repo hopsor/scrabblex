36d6ce4dfef476d925b14394a6ee1b9827ae9133
Víctor Viruete <hopsor@gmail.com>
2024-11-09 01:27:29 +0100

    clean lexicon code from the seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,21 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Scrabblex.Games.{Lexicon, LexiconEntry}
+alias Scrabblex.Accounts
 
-Scrabblex.Accounts.register_user(%{email: "john@scrabblex.org", password: "foobar", name: "john"})
-Scrabblex.Accounts.register_user(%{email: "jane@scrabblex.org", password: "foobar", name: "jane"})
-
-fise_changeset = Lexicon.changeset(%Lexicon{}, %{name: "FISE-2", language: "es", flag: "🇪🇦"})
-{:ok, fise_lexicon} = Scrabblex.Repo.insert(fise_changeset)
-
-"priv/repo/lexicons/fise-2.txt"
-|> File.stream!()
-|> Stream.chunk_every(500)
-|> Stream.each(fn chunk ->
-  batch =
-    Enum.map(chunk, &[name: String.trim(&1) |> String.upcase(), lexicon_id: fise_lexicon.id])
-
-  Scrabblex.Repo.insert_all(LexiconEntry, batch)
-end)
-|> Enum.map(fn _ -> :ok end)
+Accounts.register_user(%{email: "john@livewords.hopsor.com", password: "foobar", name: "john"})
+Accounts.register_user(%{email: "jane@livewords.hopsor.com", password: "foobar", name: "jane"})
